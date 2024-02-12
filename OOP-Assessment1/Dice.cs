4340b4d5e7dddc2351b08dbf61135e09948c3223
Unrealshadow7 <145765790+Unrealshadow7@users.noreply.github.com>
2024-02-12 15:22:23 +0000

    Add project files.

---
+++ OOP-Assessment1/Dice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assessment1
+{
+    internal class Dice
+    {
+    }
+}
