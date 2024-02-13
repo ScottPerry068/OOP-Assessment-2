4decdf96f4be424bd55692cabb7f47e843d5164d
Unrealshadow7 <scottperry068@gmail.com>
2024-02-13 19:36:26 +0000

    i have start commenting my code starting with the Dice and Game classes

--- OOP-Assessment1/Dice.cs
+++ OOP-Assessment1/Dice.cs
@@ -8,11 +8,11 @@ namespace OOP_Assessment1
 {
     public class Dice
     {
-        public int Roll()
+        public int Roll() // this is the method i use to create the roll enviroment 
         {
-            Random random = new Random();
-            int Dvalue = random.Next(1, 6);
-            return Dvalue;
+            Random random = new Random(); // this creates a class called random
+            int Dvalue = random.Next(1, 6); // this creates a random number between 1 and 6 
+            return Dvalue; // this returns the random number 
         }
     }
 }
--- OOP-Assessment1/Game.cs
+++ OOP-Assessment1/Game.cs
@@ -11,18 +11,18 @@ namespace OOP_Assessment1
     {
         public void Start()
         {
-            Dice D1 = new Dice(); Dice D2 = new Dice(); Dice D3 = new Dice();
-            int D1Face = D1.Roll();
-            Console.WriteLine("Press any key to roll the dice"); Console.ReadKey();
-            Console.WriteLine("The value of Dice one is: " + D1Face);
-            int D2Face = D2.Roll();
-            Console.WriteLine("Press any key to roll the dice"); Console.ReadKey();
-            Console.WriteLine("The value of Dice one is: " + D2Face);
-            int D3Face = D3.Roll();
-            Console.WriteLine("Press any key to roll the dice"); Console.ReadKey();
-            Console.WriteLine("The value of Dice one is: " + D3Face);
-            int result = D1Face + D2Face + D3Face;
-            Console.WriteLine("The result of the three Dice is: " + result);
+            Dice D1 = new Dice(); Dice D2 = new Dice(); Dice D3 = new Dice(); // These create my three dice objects 
+            int D1Face = D1.Roll(); //this Rolls the first dice
+            Console.WriteLine("Press any key to roll the dice"); Console.ReadKey(); // this line prompts the user to roll the dice by hitting a key on their device 
+            Console.WriteLine("The value of Dice one is: " + D1Face); // this line prints the face value for the user to see what the dice rolled
+            int D2Face = D2.Roll(); // this rolls the second dice 
+            Console.WriteLine("Press any key to roll the dice"); Console.ReadKey(); // this line prompts the user to roll the dice by hitting a key on their device
+            Console.WriteLine("The value of Dice one is: " + D2Face); // this line prints the face value for the user to see what the dice rolled
+            int D3Face = D3.Roll(); //this rolls the third dice
+            Console.WriteLine("Press any key to roll the dice"); Console.ReadKey(); // this line prompts the user to roll the dice by hitting a key on their device
+            Console.WriteLine("The value of Dice one is: " + D3Face); // this line prints the face value for the user to see what the dice rolled
+            int result = D1Face + D2Face + D3Face; // this line does the addtiion for all the faces rolled by the three dice 
+            Console.WriteLine("The result of the three Dice is: " + result); // this tells the user the result of the three faces added togther
         }
         
     }
